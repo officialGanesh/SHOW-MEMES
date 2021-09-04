f8fcd7a9021928852144b5e0e4dccb4d496a43c8
Ganesh <ganuofficial2795@gmail.com>
2021-09-04 15:31:15 +0530

    Show memes

--- memes.js
+++ memes.js
@@ -1,1 +1,34 @@
 console.log("Memes 🔥");
+
+let memeLink = '  https://meme-api.herokuapp.com/gimme';
+
+let getMeme = document.querySelector("#generate");
+let memeTitle = document.getElementById('title');
+let memeImg = document.getElementById('meme');
+
+
+getMeme.addEventListener('click',generateMeme);
+
+
+// Generating-Memes
+
+function generateMeme(){
+
+    xhr = new XMLHttpRequest();
+    xhr.open('Get',memeLink,true);
+
+    xhr.onload = function(){
+        if(this.status===200){
+
+            let objMeme = JSON.parse(this.responseText);
+            memeTitle.innerHTML = objMeme.title;
+            memeImg.src = `${objMeme.url}`
+
+        }else{
+            
+            console.log(`Something went wrong: ${e}`)
+        }
+    };
+
+    xhr.send()
+}
